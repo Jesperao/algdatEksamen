cc9fb6d24d9afe0c19271ed54b07ef991a4962a3
Jesper <j.a.ostervold@gmail.com>
2020-10-22 12:26:00 +0200

    First task is passing the test, starting work on task 2.

--- src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -5,10 +5,14 @@ import java.util.*;
 
 public class EksamenSBinTre<T> {
     public static void main (String args[]) {
-        Integer[] a = {4,7,2,9,5,10,8,1,3,6};
+        Integer[] a = {1};
         EksamenSBinTre<Integer> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
         for (int verdi : a) tre.leggInn(verdi);
-        System.out.println(tre.antall());
+        System.out.println(tre.antall()); // Utskrift: 10
+        System.out.println(tre.antall(1)); // Utskrift: 0
+        System.out.println(tre.antall(4)); // Utskrift: 3
+        System.out.println(tre.antall(7)); // Utskrift: 2
+        System.out.println(tre.antall(10)); // Utskrift: 1
     }
 
     private static final class Node<T>   // en indre nodeklasse
@@ -116,7 +120,16 @@ public class EksamenSBinTre<T> {
     }
 
     public int antall(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        int like = 0;
+        Node<T> p = rot;
+        while (p != null) {
+            int cmp = comp.compare(verdi, p.verdi);
+            if (verdi == p.verdi) {
+                like ++;
+
+            }
+        }
+        return like;
     }
 
     public void nullstill() {
